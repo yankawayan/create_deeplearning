23c249c732916bccc11fa20fe9135dbdc191c526
ac_kawasoe <kawayan6230@gmail.com>
2023-06-19 12:01:15 +0900

    0619 12:01

--- main.py
+++ main.py
@@ -1,32 +1,66 @@
 from kivy.app import App
-from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-from kivy.core.window import Window
-from kivy.graphics import Color, Line
 from kivy.uix.widget import Widget
-
-Builder.load_file('main.kv')
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.graphics import Color, Line
+from kivy.core.window import Window
+from kivy.uix.behaviors import ButtonBehavior
+# 784(28,28)  800,600->
 
 class DrawingWidget(Widget):
     def on_touch_down(self, touch):
+        x, y = touch.pos
+        print(f"Clicked at ({x}, {y})")
         if self.collide_point(*touch.pos):
-            if touch.button == 'left':
-                with self.canvas:
-                    Color(1, 0, 0, 1)
-                    touch.ud["line"] = Line(points=(touch.x, touch.y), width=50)
+            with self.canvas:
+                Color(0, 0, 0, 1)  # 黒色で描画
+                touch.ud["line"] = Line(points=(touch.x, touch.y), width=20)
 
     def on_touch_move(self, touch):
         if self.collide_point(*touch.pos):
-            if touch.button == 'left':
-                touch.ud["line"].points += (touch.x, touch.y)
+            touch.ud["line"].points += (touch.x, touch.y)
 
-class HomeWidget(BoxLayout):
-    def button_clicked(self):
-        print("Window Size", Window.size)
+    def capture_image(self,filename):
+        filename = filename + ".png"
+        self.export_to_png(filename)
 
-class FirstApp(App):
+class SavePopup(Popup):
+    def __init__(self, main_layout):
+        self.main_layout = main_layout
+        super(SavePopup, self).__init__()
+
+    def save(self):
+        filename = self.ids.filename_input.text
+        if filename:
+            self.main_layout.ids.drawing_widget.capture_image(filename)
+            self.dismiss()
+        else:
+            popup = Popup(title='Error', content=Label(text='Please enter a filename.'), size_hint=(None, None),
+                          size=(200, 150))
+            popup.open()
+
+class MainLayout(ButtonBehavior, BoxLayout):
+    def save_image(self):
+        save_popup = SavePopup(self)
+        save_popup.open()
+
+    def clear_canvas(self):
+        self.ids.drawing_widget.canvas.clear()
+
+    def show_window_size(self):
+        if self.ids.check_button.text == 'check':
+            popup = Popup(title='window size', content=Label(text=str(Window.size)), size_hint=(None, None),
+                            size=(200, 150))
+            popup.open()
+            self.ids.check_button.text = str(Window.size)
+        else:
+            self.ids.check_button.text = 'check'
+
+class HandwritingApp(App):   
     def build(self):
-        return HomeWidget()
+        return MainLayout()
+
 
 if __name__ == '__main__':
-    FirstApp().run()
+    HandwritingApp().run()
